89ad7bd72c4dcdf204602979b96bce3e0b763fc3
alpha-dever <alphadever@gmail.com>
2018-08-03 16:40:47 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          hi, I'am a React App !!!
+      </h1>
+      <Person name="Max" age="28"> my hobby is tennis </Person>
+      <Person name="Behzad" age="22"/>
+      <Person name="Jack" age="26"/>
       </div>
     );
   }
